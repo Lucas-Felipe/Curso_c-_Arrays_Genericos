c309fe1cb78d74e3a742b43653b18a2737851a2b
@lucas.felipe <l17.lucas@gmail.com>
2021-08-17 13:43:41 -0300

    usando arrays

--- Proj_Arrays_Genericos/Program.cs
+++ Proj_Arrays_Genericos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Modelos;
 
 namespace Proj_Arrays_Genericos
 {
@@ -6,7 +7,20 @@ namespace Proj_Arrays_Genericos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ContaCorrente[] contas = new ContaCorrente[3];
+
+            for (int i = 0; i < contas.Length; i++) 
+            { 
+                contas[i].Titular.Nome = $"conta{i}";
+                contas[i].Titular.CPF = i+123;
+                contas[i].Titular.Profission = $"contaprofissao{i}";
+                contas[i].NumeroAgencia = i+456;
+                contas[i].NumeroConta = i+789;
+                contas[i].Saldo = i + 1;
+                contas[i].imprime(); 
+            }
+
+
         }
     }
 }
