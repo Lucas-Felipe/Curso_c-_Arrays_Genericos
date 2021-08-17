ab2c9b395237647a409a8e12f129c03dd89cf546
@lucas.felipe <l17.lucas@gmail.com>
2021-08-17 12:39:44 -0300

    projeto inicial

---
+++ Proj_Arrays_Genericos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proj_Arrays_Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
