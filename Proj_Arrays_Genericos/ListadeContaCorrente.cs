4afabd7dde14c4f3715478f7d7567628bfc3d388
@lucas.felipe <l17.lucas@gmail.com>
2021-08-18 13:48:19 -0300

    removendo, adicionando, lista de contas

---
+++ Proj_Arrays_Genericos/ListadeContaCorrente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelos;
+
+namespace Proj_Arrays_Genericos
+{
+    public class ListadeContaCorrente
+    {
+        private ContaCorrente[] _lista;
+        private int _proximo;
+
+        public ListadeContaCorrente()
+        {
+            _lista = new ContaCorrente[] { };
+            _proximo = 0;
+        }
+
+        public void Add(ContaCorrente conta)
+        {
+            VerificaCapacidade(_proximo+1);
+            _lista[_proximo] = conta;
+            _proximo++;
+        }
+
+        private void VerificaCapacidade(int tamanho)
+        {
+            if (_lista.Length>=tamanho)
+            {
+                return;
+            }
+
+            ContaCorrente[] novoarray = new ContaCorrente[tamanho];
+            for (int i = 0; i < _lista.Length; i++)
+            {
+                novoarray[i] = _lista[i];
+            }
+            _lista = novoarray;
+        }
+
+        public void imprime() 
+        {
+            for (int i=0;i<_lista.Length;i++)
+            {
+                if (_lista[i] == null)
+                    break;
+                _lista[i].imprime();
+            }
+        }
+
+        public void remove(ContaCorrente itemexcluido)
+        {
+            int indice = -1;
+            for (int i = 0; i < _proximo; i++)
+            {
+                ContaCorrente atual = _lista[i];
+                if (atual.Equals(itemexcluido))
+                {
+                    indice = i;
+                    break;
+                }
+            }
+
+
+            for (int i = indice; i < _proximo-1; i++)
+            {
+                _lista[i] = _lista[i + 1];
+            }
+
+            _proximo--;
+            _lista[_proximo] = null;
+        }
+    }
+}
--- Proj_Arrays_Genericos/Program.cs
+++ Proj_Arrays_Genericos/Program.cs
@@ -7,16 +7,25 @@ namespace Proj_Arrays_Genericos
     {
         static void Main(string[] args)
         {
-            ContaCorrente[] contas = new ContaCorrente[3];
+            ListadeContaCorrente lista = new ListadeContaCorrente();
 
-            for (int i = 0; i < contas.Length; i++) 
-            {
-                contas[i] = new ContaCorrente();
-                contas[i].Titular = new Cliente();
-                contas[i].imprime();
-            }
+            lista.Add(new ContaCorrente("lucas",123,"sô istudante",456,789));
+            lista.Add(new ContaCorrente("pedro", 456, "vagaba", 7789, 9787));
+            lista.Add(new ContaCorrente("pedro", 456, "vagaba", 7789, 9787));
+            lista.Add(new ContaCorrente("pedro", 456, "vagaba", 7789, 9787));
+            lista.Add(new ContaCorrente("pedro", 456, "vagaba", 7789, 9787));
+            lista.Add(new ContaCorrente("pedro", 456, "vagaba", 7789, 9787));
 
+            lista.remove(new ContaCorrente("lucas", 123, "sô istudante", 456, 789));
+            lista.imprime();
+            //for (int i = 0; i < contas.Length; i++) 
+            //{
+            //    contas[i] = new ContaCorrente($"conta {i}",123+i,$"profission {i}",456+i,789+i);
 
+            //    contas[i].imprime();
+            //}
+
+            
         }
     }
 }
